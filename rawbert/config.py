c72d47fa75eab94a19c6ff83b3b8b22c75bdc103
Ryan Synk <rsynk1529@gmail.com>
2026-01-30 12:06:48 -0500

    FInalized simple version of test acc

--- rawbert/config.py
+++ rawbert/config.py
@@ -63,6 +63,7 @@ class TrainConfig:
     dataset_path: str | None = None
     test_dataset_path: str | None = None
     batch_size: int = 128
+    test_batch_size: int = 512
     lr: float = 1e-3
     backbone_lr: float = 4e-6
     total_steps: int = 1_000_000
