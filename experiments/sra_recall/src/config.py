6f156c4031640f08edabd49de8977b0c538ff9a7
Ryan Synk <rsynk1529@gmail.com>
2026-04-01 07:41:44 -0700

    Added dna2vec baseline

--- experiments/sra_recall/src/config.py
+++ experiments/sra_recall/src/config.py
@@ -39,7 +39,9 @@ class Evo2Config(AlgorithmConfig):
 
 @dataclass
 class DenseConfig(AlgorithmConfig):
-    name: Literal["rawbert", "dnabert", "generator", "neuroseed"] = "rawbert"  # "dnabert", "rawbert"
+    name: Literal["rawbert", "dnabert", "generator", "neuroseed", "dna2vec"] = (
+        "rawbert"  # "dnabert", "rawbert"
+    )
     checkpoint_path: Optional[str] = None
     batch_size: int = 128
     device: str = "cuda"
@@ -82,8 +84,16 @@ class DenseConfig(AlgorithmConfig):
             self.checkpoint: str | None = ckpt_id
             self.max_len: int = self.max_seq_len
             self.chunk_type: str = self.chunk_type
+        elif self.name == "dna2vec":
+            self.index_suffix: Path = Path("dna2vec") / config_tag
+            self.experiment_id: str = f"dna2vec_{config_tag}"
+            self.checkpoint: str | None = None
+            self.max_len: int = self.max_seq_len
+            self.chunk_type: str = self.chunk_type
         else:
-            raise ValueError(f"name expected: rawbert, dnabert, generator, or neuroseed, got = {self.name}")
+            raise ValueError(
+                f"name expected: rawbert, dnabert, generator, or neuroseed, got = {self.name}"
+            )
 
     def __str__(self):
         return self.experiment_id
--- experiments/sra_recall/src/dense_index.py
+++ experiments/sra_recall/src/dense_index.py
@@ -16,6 +16,7 @@ from Bio import SeqIO
 from torch import nn
 from tqdm import tqdm
 from transformers import (
+    AutoModel,
     AutoModelForCausalLM,
     AutoTokenizer,
     BertConfig,
@@ -345,6 +346,8 @@ class DenseEncoder:
             self._encoder = GeneratorEncoder(cfg)
         elif cfg.name == "neuroseed":
             self._encoder = NeuroSEEDEncoder(cfg)
+        elif cfg.name == "dna2vec":
+            self._encoder = DNA2VecEncoder(cfg)
         else:
             raise ValueError(f"Unknown model name: {cfg.name}")
 
@@ -636,3 +639,52 @@ class NeuroSEEDEncoder:
             batch_embeds = nn.functional.normalize(embeddings.float(), dim=1)
             embeds_list.append(batch_embeds)
         return torch.cat(embeds_list, dim=0)
+
+
+class DNA2VecEncoder:
+    # def __init__(self, model_name, batch_size, pooling, checkpoint_path, device="cuda"):
+    def __init__(self, cfg: DenseConfig):
+        transformers_logging.set_verbosity_error()
+
+        device = cfg.device
+        assert cfg.name == "dna2vec"
+        self.model = AutoModel.from_pretrained(
+            "roychowdhuryresearch/dna2vec", trust_remote_code=True
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "roychowdhuryresearch/dna2vec", trust_remote_code=True
+        )
+        self.model = self.model.eval().to(device)
+
+        self.batch_size = cfg.batch_size
+        self.device = device
+        self.pooling = cfg.pooling
+
+    @torch.no_grad()
+    def encode(self, sequences):
+        embeds_list = []
+        assert self.tokenizer
+        for batch in batched(sequences, self.batch_size):
+            tokens = self.tokenizer(batch, return_tensors="pt", padding=True).to(
+                self.device
+            )
+            outputs = self.model(**tokens)
+
+            mask = tokens.attention_mask.unsqueeze(-1)
+            if self.pooling == "class":
+                raise ValueError("dna2vec does not have a class token")
+            elif self.pooling == "mean":
+                embeddings = (outputs * mask).sum(dim=1) / mask.sum(dim=1)
+            elif self.pooling == "max":
+                mask_expanded = mask.expand(outputs.size())
+                outputs = outputs.clone()  # Prevent in-place modification warnings
+                outputs[mask_expanded == 0] = -1e9
+                embeddings, _ = outputs.max(dim=1)
+            else:
+                raise ValueError(f"self.pooling got unexpected value {self.pooling}")
+
+            batch_embeds = nn.functional.normalize(embeddings, dim=1)
+            embeds_list.append(batch_embeds)
+
+        embeddings = torch.cat(embeds_list, dim=0)
+        return embeddings
